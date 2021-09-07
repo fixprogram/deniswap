ecdbbabe1e802e00520edf39c7da183fc1c10e7a
Denis <fixprogram@mail.ru>
2021-09-07 20:21:37 +0300

    Deniswap initial part of cloning V1 of Uniswap

---
+++ test/Exchange.test.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+
+const toWei = (value) => ethers.utils.parseEther(value.toString());
+
+describe("addLiquidity", async () => {
+    it("adds liquidity", async () => {
+      await token.approve(exchange.address, toWei(200));
+      await exchange.addLiquidity(toWei(200), { value: toWei(100) });
+
+      expect(await getBalance(exchange.address)).to.equal(toWei(100));
+      expect(await exchange.getReserve()).to.equal(toWei(200));
+    });
+})
